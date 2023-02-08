61b88b9d72d4ea50b5dc6f5101b126660a4a2094
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-02-08 08:54:32 +0700

    Initial Commit

---
+++ SwiftUIAPI/SwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIApp.swift
+//  SwiftUIAPI
+//
+//  Created by Putut Yusri Bahtiar on 08/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
